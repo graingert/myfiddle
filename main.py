dfb10fa1817327ed7419abe6983ed92abf4e1e32
Thomas Grainger <tagrain@gmail.com>
2018-07-27 13:00:53 +0100

    printing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pathlib
 import django_micro
 from django.http import HttpResponse
 from django.db import models
+from django.core.files.base import ContentFile, File
 
 django_micro.configure({
     'DEBUG': True,
@@ -23,6 +24,7 @@ def homepage(request):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    file = models.FileField()
 
     class Meta:
         app_label = django_micro.get_app_label()
@@ -35,13 +37,35 @@ class PrintHelloCommand(management.BaseCommand):
     def handle(self, *args, **options):
         management.call_command('makemigrations', django_micro.get_app_label())
         management.call_command('migrate', run_syncdb=True)
-        p = Post.objects.create(title='foo')
-        Post.objects.create(parent=p, title='ham')
-        print(
+        with ContentFile('foo', name='foo') as f:
+            p = Post.objects.create(title='foo', file=f)
+
+        print('cf closed?', f.closed)
+        print('same?', f is p.file)
+        print('closed?', p.file.closed)
+        print('content', p.file.read())
+        print('closed?', p.file.closed)
+        print('cf closed?', f.closed)
+        print('#######################')
+        """
+        c = Post.objects.create(parent=p, title='ham')
+        c.file.save('foo', ContentFile('foo'))
+
+        vl = (
             Post
             .objects
-            .values_list('parent__title', 'title')
+            .values_list('parent__title', 'title', 'file')
         )
+        print(Post.objects.get(title='ham').file.read())
+        f = Post.objects.get(title='ham').file
+        with f.open('r+') as ff:
+            ff.write('hello')
+            print(ff)
+            print(f)
+            import pdb; pdb.set_trace()
+        print(ff)
+        print(f)
+        """
 
 
 application = django_micro.run()
